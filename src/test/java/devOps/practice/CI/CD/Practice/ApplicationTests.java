c60b0623d9840404c0f4483354634c3331372a11
minsub <chrkb1569@naver.com>
2023-11-13 00:43:20 +0900

    Init Project

---
+++ src/test/java/devOps/practice/CI/CD/Practice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package devOps.practice.CI.CD.Practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
