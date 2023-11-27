541d9545a7f0712d301faa18be21a7ff16e699de
minsub <chrkb1569@naver.com>
2023-11-27 22:10:14 +0900

    refactor : 출력 메시지 수정

--- src/main/java/devOps/practice/CI/CD/Practice/controller/StatusController.java
+++ src/main/java/devOps/practice/CI/CD/Practice/controller/StatusController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class StatusController {
     @GetMapping("/status")
     public String checkStatus() {
-        return "Status is Ok!";
+        return "Status is Ok!!";
     }
 }
