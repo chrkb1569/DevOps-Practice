417ada4edf0750b6e7ffc085b973bbb3ea602e52
minsub <chrkb1569@naver.com>
2024-01-03 00:31:26 +0900

    feat : 반환되는 문자열 값 변경

--- src/main/java/devOps/practice/CI/CD/Practice/controller/StatusController.java
+++ src/main/java/devOps/practice/CI/CD/Practice/controller/StatusController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class StatusController {
     @GetMapping("/status")
     public String checkStatus() {
-        return "Status is Ok!!";
+        return "WebHook Test";
     }
 
     @GetMapping("/status/v2")
